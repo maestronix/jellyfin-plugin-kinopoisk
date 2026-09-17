4f6c6e6bdb75fcd008b1aa048c28c09ff8726008
maestronix <georg.aniskin@me.com>
2026-09-17 19:22:30 +0200

    Remove API response caching from Kinopoisk service registration

--- src/Jellyfin.Plugin.Kinopoisk/KinopoiskPluginServiceRegistrator.cs
+++ src/Jellyfin.Plugin.Kinopoisk/KinopoiskPluginServiceRegistrator.cs
@@ -2,17 +2,16 @@ using System.Net.Http;
 using Jellyfin.Plugin.Kinopoisk.ProviderIdResolvers;
 using KinopoiskUnofficialInfo.ApiClient;
 using MediaBrowser.Controller;
-using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Controller.Providers;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.Kinopoisk
 {
     /// <summary>
-    /// Registers services
+    /// Registers services.
     /// </summary>
     public class KinopoiskPluginServiceRegistrator : IPluginServiceRegistrator
     {
@@ -23,11 +22,8 @@ namespace Jellyfin.Plugin.Kinopoisk
                 sp.GetRequiredService<ILogger<KinopoiskApiClient>>(),
                 sp.GetRequiredService<IHttpClientFactory>()
             ));
-            serviceCollection.AddSingleton<IKinopoiskApiClient>((sp) => new CachedKinopoiskApiClient(
-                sp.GetRequiredService<KinopoiskApiClient>(),
-                sp.GetRequiredService<IMemoryCache>(),
-                sp.GetRequiredService<ILogger<CachedKinopoiskApiClient>>()
-            ));
+
+            serviceCollection.AddSingleton<IKinopoiskApiClient>(sp => sp.GetRequiredService<KinopoiskApiClient>());
 
             serviceCollection.AddSingleton<IProviderIdResolver<MovieInfo>, VideoResolver<MovieInfo>>();
             serviceCollection.AddSingleton<IProviderIdResolver<SeriesInfo>, VideoResolver<SeriesInfo>>();
